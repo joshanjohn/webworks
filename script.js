772651a468fde3fc69a315ede70c5286ce3d72b0
joshan.john <joshan.john@student.griffith.ie>
2023-07-12 17:58:22 +0100

    hello;

--- script.js
+++ script.js
@@ -16,21 +16,27 @@ let badge = document.querySelector(".badge");
 let badgeMsg = document.querySelector(".badge span");
 
 
+let slideCount = document.getElementById("slide-count");
+
 
 let currentSlide = 0;
 
 
 window.onload = () => {
     updateSlide()
+    slideCount.innerHTML = currentSlide + 1;
 
     menuBtn.addEventListener('click', () => {
         console.log("hello");
         navBar.classList.toggle("show");
     })
-    
+
     nextBtn.addEventListener("click", () => {
         if (currentSlide < slides.length) {
             currentSlide++;
+            badgeClear();
+            slideCount.innerHTML = currentSlide + 1;
+            
             updateSlide();
         } else {
             updateSlide()
@@ -42,6 +48,8 @@ window.onload = () => {
         console.log("hello")
         if (currentSlide > 0) {
             currentSlide--;
+            badgeClear();
+            slideCount.innerHTML = currentSlide + 1;
             updateSlide();
         } else {
             updateSlide()
@@ -49,29 +57,61 @@ window.onload = () => {
 
     });
 
-    submit1.addEventListener("click", ()=>{
+    submit1.addEventListener("click", () => {
         let ans = document.getElementById("ans1");
-        if (ans.value == ".js"){
-           correctBage();
-        }else{
-            badge.classList.remove("fa-circle");
-            badge.classList.remove("fa-circle-check");
-            badge.classList.add("fa-circle-xmark");
-            badgeMsg.innerHTML = " wrong";
-            
+        if (ans.value == ".js") {
+            correctBage();
+        } else {
+            wrongBage();
+        }
+    })
+
+    submit2.addEventListener("click", () => {
+        let ans = document.getElementById("ans2");
+        console.log(ans.value)
+        if (ans.value == "a") {
+            correctBage();
+        } else {
+            wrongBage();
+        }
+    })
+
+    submit3.addEventListener("click", () => {
+        let ans = document.querySelector("input[name='ans3']:checked").value;
+        
+        if (ans == 'c') {
+            correctBage();
+        } else {
+            wrongBage();
         }
     })
 }
 
 
-function correctBage(){
+function correctBage() {
     badgeMsg.innerHTML = " correct";
     badgeMsg.style.color = "green";
     badge.style.color = "green";
     badge.classList.remove("fa-circle");
     badge.classList.remove("fa-circle-xmark");
     badge.classList.add("fa-circle-check");
-    
+}
+
+function wrongBage() {
+    badge.classList.remove("fa-circle");
+    badge.classList.remove("fa-circle-check");
+    badge.classList.add("fa-circle-xmark");
+    badgeMsg.innerHTML = " wrong";
+    badge.style.color = "red";
+    badgeMsg.style.color = "red";
+}
+
+function badgeClear() {
+    badge.style.color = "black";
+    badge.classList.remove("fa-circle-xmark");
+    badge.classList.remove("fa-circle-check");
+    badge.classList.add("fa-circle");
+    badgeMsg.innerHTML = "";
 }
 
 function updateSlide() {
